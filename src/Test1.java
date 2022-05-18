f9745e8ea64cc80228827010fddd306730d69339
joy14 <joy14479@gmail.com>
2022-05-19 05:52:07 +0900

    구현끝, clipping은 구현할까 말까

--- src/Test1.java
+++ src/Test1.java
@@ -12,12 +12,13 @@ public class Test1 {
 
 }
 
-class Cod{
+class Cod {
     public int x;
     public int y;
 
-    Cod(int x, int y){
-        this.x=x; this.y=y;
+    Cod(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
 
@@ -27,7 +28,7 @@ class Screen extends JFrame {
 
     Screen(String title) {
         super(title);
-        setSize(417,440);//사이즈
+        setSize(417, 440);//사이즈
         setDefaultCloseOperation(EXIT_ON_CLOSE); //닫을 때 끄는
         cartecian = new Cartecian();
         add(cartecian, BorderLayout.CENTER);
@@ -35,107 +36,128 @@ class Screen extends JFrame {
         setVisible(true);
     }
 }
+
 class Cartecian extends JPanel {
 
     private Graphics2D screen;
+    private int transX, transY;
 
     protected void paintComponent(Graphics g) {
-        screen=(Graphics2D)g;
+        screen = (Graphics2D) g;
         screen.translate(200, 200); //원점의 위치
+
+        transX = 0; transY = 0;
 
         drawXY();
         drawDottedLine();
 
-        //MidpointLine(5, 3, 9, 6);
-        //MidpointLine(9,6,5,3);
-        //MidpointLine(1, 1, 1, 5);
-        //MidpointLine(2, -1, 6, -1);
-        MidpointLine(2, -1, 6, -7, false);
-        MidpointLine(2, 1, 6, 7, false);
+        //기울기 양수이고 절대값이 1보다 작은 예제
+        //setTranslation(0, -1);
+        //MidpointLine(5, 3, 9, 6, false);
+
+        //setTranslation(0, 1);
+        //MidpointLine(9, 6, 5, 3, false);
+
+        //setTranslation(-3, 1);
+        //MidpointLine(1, 1, 1, 5, false);
+        //MidpointLine(2, -1, 6, -1, false);
+
+        //setTranslation(-4, 4);
+        //MidpointLine(-4, 4, 4, 4, false);
+
+        setTranslation(3, 3);
+        MidpointLine(1, 1, 3, 3, false);
+
+        //MidpointLine(2, -1, 6, -7, false);
+        //MidpointLine(2, 1, 6, 7, false);
     }
 
-    private void drawXY(){
-        float[] bold = {1,0f}; //xy축 설정
+    private void setTranslation(int x, int y){ // 원래의 선분을 검게 출력하고, 여기에 세팅된 대로 기존 선분을 Translation해서 붉은색으로 출력한다
+        this.transX = x;
+        this.transY = y;
+    }
+
+    private void drawXY() {
+        float[] bold = {1, 0f}; //xy축 설정
 
         // 굵은 선의 xy축
-        screen.setStroke(new BasicStroke(2,BasicStroke.CAP_BUTT,BasicStroke.JOIN_BEVEL,1,bold,0));
+        screen.setStroke(new BasicStroke(2, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, bold, 0));
         screen.draw(new Line2D.Float(-200, 0, 200, 0));
         screen.draw(new Line2D.Float(0, -200, 0, 200));
     }
+
     private void drawDottedLine() {
-        float[] right = {2,2f}; //점선 설정
+        float[] right = {2, 2f}; //점선 설정
 
         //점선
-        screen.setStroke(new BasicStroke(1,BasicStroke.CAP_BUTT,BasicStroke.JOIN_BEVEL,1,right,0));
-        for (int i=-200; i<=200; i = i+20) {
+        screen.setStroke(new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, right, 0));
+        for (int i = -200; i <= 200; i = i + 20) {
             screen.draw(new Line2D.Float(-200, i, 200, i));     // x축과 평행선을 그린다.
         }
-        for (int j=-200; j<=200; j = j+20) {
+        for (int j = -200; j <= 200; j = j + 20) {
             screen.draw(new Line2D.Float(j, -200, j, 200));     // y축과 평행선을 그린다.
         }
     }
 
-    public void drawDotEX(){ //예제
+    public void drawDot(int x, int y) {
+        screen.setColor(Color.BLACK);
+        screen.fillOval(x - 10, y - 10, 20, 20);
+    }
+
+    public void WritePixel(int x, int y) { // 해당 라이브러리의 y는 일반적인 값의 음수, 즉, window top에서 부터의 거리
+        screen.setColor(Color.BLACK);
+        screen.fillOval(x * 20 - 10, -(y * 20 + 10), 20, 20);
+
         screen.setColor(Color.RED);
-
-        for (int x=20, y=-20, radius=5; x<200; x=x+20,y=y-20 ) {
-            drawDot(x, y);
-        }
-    }
-    public void drawDot(int x, int y){
-        screen.setColor(Color.BLACK);
-        screen.fillOval(x-10, y-10, 20, 20);
-    }
-    public void WritePixel(int x, int y){ // 해당 라이브러리의 y는 일반적인 값의 음수, 즉, window top에서 부터의 거리
-        screen.setColor(Color.BLACK);
-        screen.fillOval(x*20-10, -(y*20+10), 20, 20);
+        screen.fillOval((x+transX) * 20 - 10, -((y+transY) * 20 + 10), 20, 20);
     }
 
-    public void MidpointLine(int x0, int y0, int x1, int y1, boolean axisToggle)
-    {
-        if(x0 > x1){ // x축을 따라 감소하는 방향으로 가는 선일경우, 스왑
+    public void MidpointLine(int x0, int y0, int x1, int y1, boolean axisToggle) {
+        if (x0 > x1) { // x축을 따라 감소하는 방향으로 가는 선일경우, 스왑
             int tempX = x0;
-            x0=x1;
+            x0 = x1;
             x1 = tempX;
             int tempY = y0;
-            y0=y1;
+            y0 = y1;
             y1 = tempY;
         }
 
         int dx = x1 - x0, dy = y1 - y0;
         double a = 0;
-        if(dx != 0){ // dx가 0이 아니면 a값을 구해줌
-            a = (float)dy/(float)dx;
-        } else if(x1 == x0){ // x값이 같은경우, 수직선, 기울기 a는 정의되지않음
-            int smallY=0, bigY=0;
-            if(y0<y1){
-                smallY = y0; bigY=y1;
+        if (dx != 0) { // dx가 0이 아니면 a값을 구해줌
+            a = (float) dy / (float) dx;
+        } else if (x1 == x0) { // x값이 같은경우, 수직선, 기울기 a는 정의되지않음
+            int smallY = 0, bigY = 0;
+            if (y0 < y1) {
+                smallY = y0;
+                bigY = y1;
+            } else {
+                smallY = y1;
+                bigY = y0;
             }
-            else {
-                smallY = y1; bigY=y0;
-            }
-            while(smallY <= bigY){
+            while (smallY <= bigY) {
                 WritePixel(x0, smallY);
                 smallY++;
             }
             return;
         }
 
-        if(Math.abs(a) < 1) {
+        if (Math.abs(a) <= 1) {
             int toggle = 1;
-            if(a < 0){
+            if (a < 0) { // 기울기가 음수라면 선분이 x축에 대칭인것처럼 결정짓고 다시 대칭시켜 구한다
                 toggle = -1;
-                y0 = -y0; y1 = -y1;
+                y0 = -y0;
+                y1 = -y1;
                 dy = -dy;
             }
 
             int incrE, incrNE, d, x, y;
-            d = dy*2 - dx; // 시작 결정 변수에 x2를 하여 fraction이 방지된 형태의 초기화
-            incrE = dy*2; // E점 채용시 결정변수 증가량에 x2를 하여 fraction 방지
-            incrNE = (dy-dx)*2; // NE점 채용시 결정변수 증가량에 x2를 하여 fraction 방지
+            d = dy * 2 - dx; // 시작 결정 변수에 x2를 하여 fraction이 방지된 형태의 초기화
+            incrE = dy * 2; // E점 채용시 결정변수 증가량에 x2를 하여 fraction 방지
+            incrNE = (dy - dx) * 2; // NE점 채용시 결정변수 증가량에 x2를 하여 fraction 방지
             x = x0;
             y = y0;
-            if(!axisToggle) {
+            if (!axisToggle) { // 기울기 1이상의 경우 x, y값을 바꾸어 y축에 따른 x값으로 d를 결정지었으므로, 출력 때 다시 축을 바꿔주는 작업을 수행한다
                 WritePixel(x, y * toggle); // 시작하는 픽셀
                 while (x < x1) { // x축을 따라 전진
                     if (d <= 0) { //E를 집었을 때
@@ -148,7 +170,7 @@ class Cartecian extends JPanel {
                     }
                     WritePixel(x, y * toggle); /* The selected pixel closest to the line */
                 }
-            } else{
+            } else {
                 WritePixel(y * toggle, x); // 시작하는 픽셀
                 while (x < x1) { // x축을 따라 전진
                     if (d <= 0) { //E를 집었을 때
@@ -162,10 +184,10 @@ class Cartecian extends JPanel {
                     WritePixel(y * toggle, x); /* The selected pixel closest to the line */
                 }
             }
-        } else if(Math.abs(a) > 1){
+        } else if (Math.abs(a) > 1) {
             MidpointLine(y0, x0, y1, x1, true);
         }
-        
+
 
     }
 }
